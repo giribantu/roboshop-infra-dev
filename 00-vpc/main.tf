43c7c9b4220a0b09416fbbd7a0f957b2bd10be2c
giribantu <giribantudevops@gmail.com>
2025-07-02 17:49:43 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -15,3 +15,8 @@ module "vpc"{
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "subnet_ids" {
+ value= module.vpc.public_subnet_ids  
+
+}
--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -9,3 +9,25 @@ module "frontend"{
     vpc_id = local.vpc_id
 
 }
+
+module "bastion"{
+    /* source = "../../terraform-aws-securitygroup" */
+    source = "git::https://github.com/giribantu/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name =var.bastion_sg_name
+    sg_description = var.bastion_sg_description
+    vpc_id = local.vpc_id
+
+}
+
+#bastion accepting connection from my laptop
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
+}
