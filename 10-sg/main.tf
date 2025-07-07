cccc704c0a1c70d3f3cb428a2cea97fccb7feba0
giribantu <giribantudevops@gmail.com>
2025-07-07 21:15:33 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -45,6 +45,55 @@ module "vpn"{
     vpc_id = local.vpc_id
 
 }
+
+module "mongodb"{
+    /* source = "../../terraform-aws-securitygroup" */
+    source = "git::https://github.com/giribantu/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name ="mongodb"
+    sg_description = "for mongodb"
+    vpc_id = local.vpc_id
+
+}
+
+module "redis"{
+    /* source = "../../terraform-aws-securitygroup" */
+    source = "git::https://github.com/giribantu/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name ="redis"
+    sg_description = "for redis"
+    vpc_id = local.vpc_id
+
+}
+
+module "mysql"{
+    /* source = "../../terraform-aws-securitygroup" */
+    source = "git::https://github.com/giribantu/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name ="mysql"
+    sg_description = "for mysql"
+    vpc_id = local.vpc_id
+
+}
+
+module "rabbitmq"{
+    /* source = "../../terraform-aws-securitygroup" */
+    source = "git::https://github.com/giribantu/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name ="rabbitmq"
+    sg_description = "for rabbitmq"
+    vpc_id = local.vpc_id
+
+}
+
 #bastion accepting connection from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
@@ -110,4 +159,50 @@ resource "aws_security_group_rule" "backend_alb_vpn" {
   protocol          = "tcp"
   source_security_group_id =module.vpn.sg_id
   security_group_id = module.backend_alb.sg_id
+}
+
+#mongodb accepting connection from vpn on port no 22 and 27017
+resource "aws_security_group_rule" "mongodb_vpn" {
+
+  count = length(var.mongodb_ports_vpn)
+  type              = "ingress"
+  from_port         = var.mongodb_ports_vpn[count.index]
+  to_port           = var.mongodb_ports_vpn[count.index]
+  protocol          = "tcp"
+  source_security_group_id =module.vpn.sg_id
+  security_group_id = module.mongodb.sg_id
+}
+
+
+resource "aws_security_group_rule" "redis_vpn" {
+
+  count = length(var.redis_ports_vpn)
+  type              = "ingress"
+  from_port         = var.redis_ports_vpn[count.index]
+  to_port           = var.redis_ports_vpn[count.index]
+  protocol          = "tcp"
+  source_security_group_id =module.vpn.sg_id
+  security_group_id = module.redis.sg_id
+}
+
+resource "aws_security_group_rule" "mysql_vpn" {
+
+  count = length(var.mysql_ports_vpn)
+  type              = "ingress"
+  from_port         = var.mysql_ports_vpn[count.index]
+  to_port           = var.mysql_ports_vpn[count.index]
+  protocol          = "tcp"
+  source_security_group_id =module.vpn.sg_id
+  security_group_id = module.mysql.sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq_vpn" {
+
+  count = length(var.rabbitmq_ports_vpn)
+  type              = "ingress"
+  from_port         = var.rabbitmq_ports_vpn[count.index]
+  to_port           = var.rabbitmq_ports_vpn[count.index]
+  protocol          = "tcp"
+  source_security_group_id =module.vpn.sg_id
+  security_group_id = module.rabbitmq.sg_id
 }
---
+++ 30-vpn/main.tf
@@ -0,0 +1,21 @@
+resource "aws_key_pair" "openvpn" {
+  key_name   = "openvpn"
+  public_key = file("E:\\Devops\\openvpn.pub") # for maxc or linux use /
+}
+
+
+resource "aws_instance" "vpn" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.vpn_sg_id]
+  subnet_id = local.public_subnet_id
+  /* key_name = "aws_84s.pub" #make sure key is exist in AWS */
+  key_name = aws_key_pair.openvpn.key_name
+  user_data= file("openvpn.sh")
+  tags =merge(
+    local.common_tags,
+    {
+        Name="${var.project}-${var.environment}-vpn"
+    }
+  )
+  }
