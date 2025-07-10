1bc9515ea08075c28daf48e17af5d9fac9ae1094
giribantu <giribantudevops@gmail.com>
2025-07-10 17:49:16 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -94,6 +94,18 @@ module "rabbitmq"{
 
 }
 
+module "catalogue"{
+    /* source = "../../terraform-aws-securitygroup" */
+    source = "git::https://github.com/giribantu/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name ="catalogue"
+    sg_description = "for catalogue"
+    vpc_id = local.vpc_id
+
+}
+
 #bastion accepting connection from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
@@ -205,4 +217,50 @@ resource "aws_security_group_rule" "rabbitmq_vpn" {
   protocol          = "tcp"
   source_security_group_id =module.vpn.sg_id
   security_group_id = module.rabbitmq.sg_id
+}
+
+#catalogue should open these below ports
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id =module.backend_alb.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id =module.vpn.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_http" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id =module.vpn.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id =module.bastion.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+#mogodb should allow catalogue as input
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  source_security_group_id =module.catalogue.sg_id
+  security_group_id = module.mongodb.sg_id
 }
--- 50-backend-lb/variables.tf
+++ 50-backend-lb/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z0352408257N4PSXSNZ2I"
+}
+
+variable "zone_name" {
+  default = "giribantudevops.site"
+}
